ad0edd229626c8f9863cba7a23cd46f92ccf1396
MinigamesKid-git <londonboom5@gmail.com>
2026-07-09 19:53:41 -0400

    Update fpsbooster.lua

--- fpsbooster.lua
+++ fpsbooster.lua
@@ -1,29 +1,12 @@
-local Terrain = game:GetService("Workspace"):FindFirstChildOfClass("Terrain")
-if Terrain then
-    Terrain.WaterWaveSize = 0
-    Terrain.WaterWaveSpeed = 0
-    Terrain.WaterReflectance = 0
-    Terrain.WaterTransparency = 0
-end
-
--- Change global lighting settings to lower GPU render load
-local Lighting = game:GetService("Lighting")
-Lighting.GlobalShadows = false
-Lighting.FogEnd = 9e9
-settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
-
--- Clear graphic textures and effects from every object in the game
-for _, v in pairs(game:GetDescendants()) do
-    if v:IsA("Part") or v:IsA("CornerWedgePart") or v:IsA("WedgePart") or v:IsA("TrussPart") then
-        v.Material = Enum.Material.SmoothPlastic
-        v.Reflectance = 0
-    elseif v:IsA("Decal") or v:IsA("Texture") then
-        v:Destroy() -- Removes custom image textures from walls and floors
-    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") or v:IsA("Smoke") or v:IsA("Sparkles") or v:IsA("Fire") then
-        v:Destroy() -- Disables laggy visual particles
-    elseif v:IsA("PostEffect") or v:IsA("BloomEffect") or v:IsA("BlurEffect") or v:IsA("ColorCorrectionEffect") or v:IsA("SunRaysEffect") then
-        v:Destroy() -- Turns off screen blur, bloom, and sunrays
-    end
-end
-
-print("FPS Booster fully optimized and injected!")
+{
+  "FFlagDebugDisableDeviceAnisotropy": "True",
+  "FFlagRenderShadowIntensity": "0",
+  "DFStringTextureQualityOverride": "0",
+  "FIntRenderShadowMapBias": "0",
+  "FFlagFastLightingDynamic2": "False",
+  "FIntFRMMinFrameTime": "0",
+  "FIntFRMMaxFrameTime": "0",
+  "FIntGraphicsQualityLevel": "1",
+  "FFlagDebugGraphicsPreferVulkan": "True",
+  "DFIntTaskSchedulerTargetFps": "999"
+}
